e361ed47fac82b333d021d8f2bbbd1e5fe727332
Szymon <szymon.wiercigroch@gmail.com>
2020-05-05 15:59:03 +0200

    Lol działa XD

--- Surce_Code/Main.py
+++ Surce_Code/Main.py
@@ -1,5 +1,9 @@
 from Model import *
 
-if(__name__=='__main__'):
+if (__name__ == '__main__'):
 
-    print("hello")
+    battle = Battlefield(1,1,1,5)
+    for i in range(3):
+        battle.step()
+    for aagent in battle.schedule.agents:
+        print(aagent.health)
--- Surce_Code/Model.py
+++ Surce_Code/Model.py
@@ -13,37 +13,44 @@ class Battlefield(Model):
         self.running = True
 
         for i in range(self.numerical_army_1):
+            a = Infantry(i, self)
+            self.schedule.add(a)
 
-            # TODO
-            #dodawanie 1
-
+            self.grid.place_agent(a, (0, 0))
 
         for j in range(self.numerical_army_2):
+            a = Infantry(10, self)
+            self.schedule.add(a)
 
-            # TODO
-            #dodawanie 2
+            self.grid.place_agent(a, (0, 3))
 
     def step(self):
         self.schedule.step()
 
+
 class Infantry(Agent):
-    def __init__(self,id,model):
-        super().__init__(self,id,model)
+    def __init__(self, id, model):
+        super().__init__(id, model)
         self.health = 10
         self.attack = 3
 
     def move(self):
-        #bardzo uproszczone tak tylko żeby na siebie szli
+        # bardzo uproszczone tak tylko żeby na siebie szli
 
-        if(self.pos[1]>1):
-            self.model.grid.move_agent(self,(0,self.pos[1]-1))
+        if self.pos[1] > 1:
+            self.model.grid.move_agent(self, (0, self.pos[1] - 1))
         else:
             self.model.grid.move_agent(self, (0, self.pos[1] + 1))
 
     def fight(self):
         oponents = self.model.grid.get_cell_list_contents([self.pos])
-        if len(oponents)>1:
+        if len(oponents) > 1:
+            other = self.random.choice(oponents)
+            other.health = other.health - self.attack
+            self.health = self.health - other.attack
 
-
-
-
+    def step(self):
+        if (self.pos[1] != 1):
+            self.move()
+        else:
+            self.fight()
