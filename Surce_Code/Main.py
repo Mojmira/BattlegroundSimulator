90dc403e72a686991b20930f7e5e8f0e72ba128e
Szymon <szymon.wiercigroch@gmail.com>
2020-05-06 15:56:15 +0200

    Małe zmiany w modelu, movement change na bardziej skomplikowany, implementacja obserwacji wokół sibie, i chyba tyle XD

--- Surce_Code/Main.py
+++ Surce_Code/Main.py
@@ -1,9 +1,18 @@
 from Model import *
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
 
-    battle = Battlefield(1,1,1,5)
-    for i in range(3):
+    battle = Battlefield(2, 20, 20)
+    for i in range(20):
         battle.step()
     for agent in battle.schedule.agents:
-        print(agent.health)
+        print(agent.scout())
+
+    agent_counts = np.zeros((battle.grid.width, battle.grid.height))
+    for cell in battle.grid.coord_iter():
+        cell_content, x, y = cell
+        agent_count = len(cell_content)
+        agent_counts[x][y] = agent_count
+    plt.imshow(agent_counts, interpolation='nearest')
+    plt.colorbar()
+    plt.show()
--- Surce_Code/Model.py
+++ Surce_Code/Model.py
@@ -2,28 +2,27 @@ from mesa import Model
 from mesa import Agent
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Battlefield(Model):
-    def __init__(self, army_1, army_2, width, height):
+    def __init__(self, army_1, width, height):
         super().__init__()
         self.numerical_army_1 = army_1
-        self.numerical_army_2 = army_2
-        self.grid = MultiGrid(width, height, False)
+        self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running = True
 
+        i = 0
         for i in range(self.numerical_army_1):
             a = Infantry(i, self)
             self.schedule.add(a)
 
-            self.grid.place_agent(a, (0, 0))
+            x = self.random.randint(0, width - 1)
+            y = self.random.randint(0, height - 1)
 
-        for j in range(self.numerical_army_2):
-            a = Infantry(10, self)
-            self.schedule.add(a)
-
-            self.grid.place_agent(a, (0, 3))
+            self.grid.place_agent(a, (x, y))
 
     def step(self):
         self.schedule.step()
@@ -34,29 +33,61 @@ class MainAgent(Agent):
         super().__init__(id, model)
         self.health = 0
         self.attack = 0
+        self.model = model
+
+    def scout(self):
+        field = self.model.grid.get_neighbors(
+            self.pos,  # Pozycja jednostki
+            True,  # True=Moore neighborhood False=Von Neumann neighborhood
+            False,  # Srodek
+            7  # Promien
+        )
+        return field
 
     def move(self):
-        # bardzo uproszczone tak tylko żeby na siebie szli
 
-        if self.pos[1] > 1:
-            self.model.grid.move_agent(self, (0, self.pos[1] - 1))
+        others = self.scout()
+
+        if len(others) < 1:
+            print("I'm mooving hehe")
+            fields_around = self.model.grid.get_neighborhood(
+                self.pos,
+                True,
+                False,
+                2
+            )
+            self.model.grid.move_agent(self, self.random.choice(fields_around))
         else:
-            self.model.grid.move_agent(self, (0, self.pos[1] + 1))
+            print("I'm in touch with somebody ")
+            new_pos = [0, 0]
+
+            if others[0].pos[0]>self.pos[0]:
+                new_pos[0] = self.pos[0]+1
+            elif others[0].pos[0]<self.pos[0]:
+                new_pos[0] = self.pos[0] - 1
+            else:
+                pass
+
+            if others[0].pos[1]>self.pos[1]:
+                new_pos[1] = self.pos[1] + 1
+            elif others[0].pos[1]<self.pos[1]:
+                new_pos[1] = self.pos[1] - 1
+            else:
+                pass
+
+            new_pos_tup = (new_pos[0], new_pos[1])
+            self.model.grid.move_agent(self, new_pos_tup)
 
     def fight(self):
         oponents = self.model.grid.get_cell_list_contents([self.pos])
         if len(oponents) > 1:
-
-            #bije siebie też XDDD
+            # bije siebie też XDDD
             other = self.random.choice(oponents)
             other.health = other.health - self.attack
             self.health = self.health - other.attack
 
     def step(self):
-        if self.pos[1] != 1:
-            self.move()
-        else:
-            self.fight()
+        self.move()
 
 
 class Infantry(MainAgent):
